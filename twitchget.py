f4291020a7807cb8cc1f473b0f8ad9f8ffc7034b
David Sanders <david.wm.sanders@gmail.com>
2020-03-14 01:40:49 +0000

    Detect and handle new cookie value encryption method v10

--- twitchget.py
+++ twitchget.py
@@ -2,20 +2,37 @@
 
 Usage: twitchget.py <target> <download_dir>
 """
+import base64
+import json
 import shutil
 import sqlite3
 import subprocess
 from pathlib import Path
 
 import win32crypt
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from docopt import docopt
 
 
 SCRIPT_DIR = Path(__file__).parent.resolve()
+CHROME_USERDATA_PATH = Path.home() / "AppData/Local/Google/Chrome/User Data"
+CHROME_LOCALSTATE_PATH = CHROME_USERDATA_PATH / "Local State"
+CHROME_COOKIES_PATH = CHROME_USERDATA_PATH / "Default/Cookies"
 HOST_KEY = ".twitch.tv"
 
 
-def _get_cookies(cookies_db_path, host_key):
+def _get_encryption_key(chrome_local_state_path):
+    # Open the Chrome Local State as json and get the os_crypt.encrypted_key
+    with chrome_local_state_path.open("r", encoding="utf-8") as f:
+        base64_encrypted_key = json.load(f)["os_crypt"]["encrypted_key"]
+    # Decode the encrypted key from base64 and remove the "DPAPI" marker at the start
+    encrypted_key = base64.b64decode(base64_encrypted_key)[5::]
+    # Decrypt the encrypted key
+    key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
+    return key
+
+
+def _get_cookies(cookies_db_path, host_key, aes_gcm_key):
     cookies = []
     # Copy the cookies database locally to avoid hitting a lock if Chrome has its copy open
     cookies_db_copyp = shutil.copy(cookies_db_path, SCRIPT_DIR)
@@ -26,9 +43,14 @@ def _get_cookies(cookies_db_path, host_key):
     params = (host_key,)
     cursor.execute("SELECT name, path, expires_utc, is_secure, encrypted_value FROM cookies WHERE host_key=?", params)
     for result in cursor.fetchall():
-        name, path, expiry, secure, ev = result[0:5]
+        name, path, expiry, secure, ev = result
         # Decrypt the cookie value
-        dv = win32crypt.CryptUnprotectData(ev, None, None, None, 0)[1].decode("utf-8")
+        if ev.startswith(b"v10"):
+            nonce, cipertext = ev[3:15], ev[15:]
+            aes_gcm = AESGCM(aes_gcm_key)
+            dv = aes_gcm.decrypt(nonce, cipertext, None).decode("utf-8")
+        else:
+            dv = win32crypt.CryptUnprotectData(ev, None, None, None, 0)[1].decode("utf-8")
         cookies.append((name, path, expiry, secure, dv))
     # Close the database cleanly
     db.close()
@@ -56,7 +78,8 @@ def _write_cookies_file(cookies):
             name, path, expiry, secure, dv = cookie
             # Convert the secure integer to TRUE/FALSE
             secure = "TRUE" if secure else "FALSE"
-            cf.write(f"{HOST_KEY}\tTRUE\t{path}\t{secure}\t{expiry}\t{name}\t{dv}\n")
+            if dv:
+                cf.write(f"{HOST_KEY}\tTRUE\t{path}\t{secure}\t{expiry}\t{name}\t{dv}\n")
     return cookies_txt_p
 
 
@@ -70,9 +93,10 @@ if __name__ == '__main__':
     args = docopt(__doc__)
     target, download_dir = args["<target>"], args["<download_dir>"]
 
-    chrome_cookies_p = Path.home() / "AppData/Local/Google/Chrome/User Data/Default/Cookies"
+    # Extract and decrypt the os_crypt.encrypted_key from Chrome Local State
+    aes_gcm_key = _get_encryption_key(CHROME_LOCALSTATE_PATH)
     # Get current cookies for ".twitch.tv" from Chrome default profile Cookies sqlite database
-    cookies = _get_cookies(chrome_cookies_p, HOST_KEY)
+    cookies = _get_cookies(CHROME_COOKIES_PATH, HOST_KEY, aes_gcm_key)
     # Filter out cookies that are not wanted and/or required
     cookies = _filter_cookies(cookies)
     # Write the cookies to a youtube-dl compliant Netscape cookies.txt format
